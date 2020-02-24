01c637afe7bde5fdfa10dbda4c80d70c93b11c7b
Michael Passade <michael.passade@codeimmersives.com>
2020-02-24 15:11:55 -0500

    done

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-function isHot() {
+function isHot(temp) {
+ return temp >= 80 ? 'Yes, it is indeed hot.' : 'No, it is not hot.'
+}
 
+function helloThere(name) {
+  return name.length>5 ? 'Hi, ' + name : 'Hello, ' + name
 }
 
 
-function helloThere() {
-
-}
-
-
-function goodbyeYou() {
-
+function goodbyeYou(name) {
+  return name ? 'Goodbye, '+name : 'Goodbye, stranger'
 }
 
 
